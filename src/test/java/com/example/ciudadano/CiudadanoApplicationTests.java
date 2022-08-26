aed461cd0212a8e0804f9b3f019f01e341b43798
Jalejandro2000 <jairo.angulo2017@uteq.edu.ec>
2022-08-26 14:53:02 -0500

    voki

---
+++ src/test/java/com/example/ciudadano/CiudadanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ciudadano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiudadanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
